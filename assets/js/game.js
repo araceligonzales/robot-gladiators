3d46a86b275c5b5119ac59cfe8c35c46761309f4
cell gonzales <aragonzales.k@gmail.com>
2022-02-09 13:50:34 -0800

    conditional statements for checking player/enemy health

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,47 @@
 var playerName = window.prompt("What is your robot's name?");
-
-//this will confirm robo name to player
-window.alert(playerName)
 console.log(playerName)
-//creating a funtion named fight 
 
-function fight() {
-    window.alert("The fight has begun!");
-  }
+//variables for player stats
+var playerHealth = 100 ;
+var playerAttack = 10 ;
 
-  //calling the fight function
-  //fight();
+//variables for enemy stats
+var enemyName = "Rafael the Robot";
+var enemyHealth = 50 ;
+var enemyAttack = 12 ;
+
+var fight = function() {
+    //tells player round is starting
+ window.alert("Welcome to Robot Gladiators Nerd!");
+};
+fight();
+
+///subtract playerAttack value to enemyHealth value and use result to update enemyHealth value
+enemyHealth = enemyHealth - playerAttack;
+
+//log new result to confirm it worked
+console.log(
+    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + "health remaining.");
+
+    // check enemy health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+//subtract enemyAttack value to playerHealth value and use result to update new playerHealth value
+playerHealth = playerHealth - enemyAttack ;
+
+//log result to confirm
+console.log(
+    enemyName + " attacked " + playerName + " . " + playerName + " now has " + playerHealth + "health remaining.")
+
+    //check players health
+    if (playerHealth <=0){
+        window.alert(playerName + "has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }

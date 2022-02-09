875c32c52e8b95e1e8ec8cd63f230a1f07a720f3
cell gonzales <aragonzales.k@gmail.com>
2022-02-08 18:03:18 -0800

    js functions to communicate with player

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+
+//this will confirm robo name to player
+window.alert(playerName)
+console.log(playerName)
+//creating a funtion named fight 
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+  //calling the fight function
+  //fight();
